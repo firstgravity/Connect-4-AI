c5108a5775599696f421fd1b595f38162c703cf4
firstgravity <maxime@familleberger.fr>
2026-09-24 18:35:02 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,26 +4,33 @@ class Window(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.width = 400
-        self.height = 400
+        self.width = 600
+        self.height = 600
 
         self.canva = self.init_canva((8, 8))
 
-        self.geometry("400x400")
+        self.geometry(f"{self.width}x{self.height}")
         self.title("Connect 4")
 
     def init_canva(self, size):
         canva = tk.Canvas(self)
-        canva.pack(fill=tk.BOTH, expand=tk.YES)
-
+        canva.pack(fill="both", expand=tk.YES)
         canva.bind("<Configure>", self.on_resize)
-
         return canva
 
     def on_resize(self, event):
+
+        # Update the dimension
         self.height = event.height
         self.width = event.width
+
+        ## Background reset
+        self.canva.create_rectangle(0, 0, self.width, self.height, fill='white')
+
+        ## Background game board
         self.canva.create_rectangle(30 , 30, 300, 300, fill='grey', outline='')
+
+        ## Empty cells
         '''distance = min(self.winfo_reqwidth(), self.winfo_reqheight()) * 0.17
         initial = []
         h = [30, 30, 30 + distance * 0.7, 30 + distance * 0.7]
